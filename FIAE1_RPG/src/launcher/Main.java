584983e40893b63b17faeeadd3145ec9464d62b4
L2P-Academy <projekte@l2p-academy.com>
2024-04-30 16:20:59 +0200

    Beispielfenster, Struktur MVC, module-info

--- FIAE1_RPG/src/launcher/Main.java
+++ FIAE1_RPG/src/launcher/Main.java
@@ -1,9 +1,14 @@
 package launcher;
 
+import view.CharacterView;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Das wird ein RPG der Extraklasse! :D");
+		
+		// Anzeige des Fensters als Beispiel
+		CharacterView view = new CharacterView();
 
 	}
 
