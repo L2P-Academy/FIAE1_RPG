5f237a6edd734b20cd08ae54e99466c4803869bc
Velsabor <rfnew22@outlook.de>
2024-05-02 15:05:45 +0200

    test code

--- FIAE1_RPG/src/model/BossModel.java
+++ FIAE1_RPG/src/model/BossModel.java
@@ -1,5 +1,36 @@
-package model;
-
 public class BossModel {
+    private List<MobModel> minions;
+    private int phase;
 
+    public BossModel(List<MobModel> minions, int phase) {
+        this.minions = minions;
+        this.phase = phase;
+    }
+
+    // Methoden entsprechend dem UML-Diagramm
+    public int getPhase() {
+        return phase;
+    }
+
+    public void setPhase(int phase) {
+        this.phase = phase;
+    }
+
+    public List<MobModel> getMinions() {
+        return minions;
+    }
+
+    public void setMinions(List<MobModel> minions) {
+        this.minions = minions;
+    }
+
+    // Beispielmethoden
+    public void attack() {
+        // Implementierung für den Angriff des Bosses
+    }
+
+    public int calculateDamage() {
+        // Implementierung für die Schadensberechnung des Bosses
+        return 0;
+    }
 }
