b26009f29034f0d5ca10116e575515d50e29903b
Rakinos89 <rakinos89@yahoo.com>
2024-05-02 12:53:07 +0200

    Added classes CharacterModel and MobModel

---
+++ FIAE1_RPG/src/model/MobModel.java
@@ -0,0 +1,47 @@
+package model;
+
+public class MobModel extends CharacterModel {
+    private boolean isFriendly;
+    private int level;
+    private boolean isFlying;
+
+    // Constructor
+    public MobModel(String name, String race, int healthPoints, int baseDmg, int baseArmour, boolean isFriendly, int level, boolean isFlying) {
+        super(name, race, healthPoints, baseDmg, baseArmour);
+        this.isFriendly = isFriendly;
+        this.level = level;
+        this.isFlying = isFlying;
+    }
+    
+    // Method to display extra mob information
+    public void displayMobInfo() {
+    	System.out.println("Friendly: " + isFriendly);
+    	System.out.println("Level: " + level);
+    	System.out.println("Flying: " + isFlying);
+    }
+
+    // Getters and Setters
+    public boolean isFriendly() {
+        return isFriendly;
+    }
+
+    public void setFriendly(boolean friendly) {
+        isFriendly = friendly;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isFlying() {
+        return isFlying;
+    }
+
+    public void setFlying(boolean flying) {
+        isFlying = flying;
+    }
+}
