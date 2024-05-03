c45b7ebfe763adabe9b7950e1bf366e16808c57c
weirdmustard <samael.schroeder@mail.de>
2024-05-03 12:27:31 +0200

    constructor, info & getter/setter added

--- FIAE1_RPG/src/model/ItemModel.java
+++ FIAE1_RPG/src/model/ItemModel.java
@@ -1,20 +1,89 @@
 package model;
 
-import javax.swing.JList;
-
 public class ItemModel {
+    private String itemName;
+    private String itemDurability;
+    private int itemQuantity;
+    private int itemPrice;
+    private int itemDamage;
+    
+	// Fragen? -> SAMAEL SCHRÖDER
 	
-	// Übernommen von Samael
-	
-	String name;
+	// aus dem Modell übernommene Eigenschaften
+    
+	/*String name;
 	int durability;
 	int quantity;
 	double price;
 	boolean isQuestItem;
 	int damage;
-	JList <String> category;
-	
-	// aus dem Modell übernommene Eigenschaften
-	
+	JList <String> category;*/
 
-}
+	// Constructor
+	
+	   public ItemModel(String itemName, String itemDurability, int itemQuantity, int itemPrice, int itemDamage) {
+	        this.itemName = itemName;
+	        this.itemDurability = itemDurability;
+	        this.itemQuantity = itemQuantity;
+	        this.itemPrice = itemPrice;
+	        this.itemDamage = itemDamage;
+	        
+	   }
+	   
+	   // Display Item info
+	   
+	   public void displayIteminfo() {
+	        System.out.println("Name: " + itemName);
+	        System.out.println("Zustand: " + itemDurability);
+	        System.out.println("Schaden: " + itemDamage);
+	        System.out.println(" ");
+	        System.out.println("Anzahl: " + itemQuantity);
+	        System.out.println(" ");
+	        System.out.println("Kosten: " + itemPrice);
+	        
+	   }
+	   
+	   // Getter & Setter
+	   
+	   public String getItemName() {
+	        return itemName;
+	    }
+
+	    public void setItemName(String itemName) {
+	    	this.itemName = itemName;
+	    }
+	    
+	    public String itemDurability() {
+	        return itemDurability;
+	    }
+
+	    public void itemDurability(String itemDurability) {
+	    	this.itemDurability = itemDurability;
+	    }
+	    
+	    public int itemQuantity() {
+	        return itemQuantity;
+	    }
+
+	    public void itemQuantity(int itemQuantity) {
+	    	this.itemQuantity = itemQuantity;
+	    }
+	    
+	    public int itemPrice() {
+	        return itemQuantity;
+	    }
+
+	    public void itemPrice(int itemPrice) {
+	    	this.itemPrice = itemPrice;
+	    }
+	    
+	    public int itemDamage() {
+	        return itemDamage;
+	    }
+
+	    public void itemDamage(int itemDamage) {
+	    	this.itemDamage = itemDamage;
+	    }
+	    
+	}
+	    
