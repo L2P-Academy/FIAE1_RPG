f3b36642debedc4bdc6b8cd41b6812cf227936bc
L2P-Academy <projekte@l2p-academy.com>
2024-05-03 10:10:03 +0200

    Fixed Errors

--- FIAE1_RPG/src/model/BossModel.java
+++ FIAE1_RPG/src/model/BossModel.java
@@ -1,3 +1,7 @@
+package model;
+
+import java.util.List;
+
 public class BossModel {
     private List<MobModel> minions;
     private int phase;
--- FIAE1_RPG/src/model/NpcCharacterModel.java
+++ FIAE1_RPG/src/model/NpcCharacterModel.java
@@ -2,10 +2,14 @@ package model;
 
 public class NpcCharacterModel extends CharacterModel{
 	
+	public NpcCharacterModel(String name, String race, int healthPoints, int baseDmg, int baseArmour) {
+		super(name, race, healthPoints, baseDmg, baseArmour);
+		// TODO Auto-generated constructor stub
+	}
+	
 	boolean isFriendly;
 	boolean isTrader;
 	boolean isQuestGiver;
 	
 	
 	}
-}
