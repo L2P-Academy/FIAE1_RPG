08de18e4c1a9c8984056f188aa8df1149206b662
weirdmustard <samael.schroeder@mail.de>
2024-05-03 10:12:45 +0200

    Eigenschaften aus Modell hinterlegt

--- FIAE1_RPG/src/model/ItemModel.java
+++ FIAE1_RPG/src/model/ItemModel.java
@@ -1,7 +1,20 @@
 package model;
 
+import javax.swing.JList;
+
 public class ItemModel {
 	
 	// Übernommen von Samael
+	
+	String name;
+	int durability;
+	int quantity;
+	double price;
+	boolean isQuestItem;
+	int damage;
+	JList <String> category;
+	
+	// aus dem Modell übernommene Eigenschaften
+	
 
 }
