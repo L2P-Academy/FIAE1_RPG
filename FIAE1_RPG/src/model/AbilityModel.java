bf34743ff2a2eaba9779ec957d283eac9b40198e
antcedric91 <ant.cedric91@gmail.com>
2024-05-03 09:07:09 +0200

    Added more constructors and Getters and Setters

--- FIAE1_RPG/src/model/AbilityModel.java
+++ FIAE1_RPG/src/model/AbilityModel.java
@@ -1,14 +1,25 @@
-package model;
+package model; // v0.2
 
 import java.util.ArrayList;
 
 public class AbilityModel {
 	
 	boolean isRanged, isAOE;
-	ArrayList<String> elementsList;
+	ArrayList<String> elementsList; // neutral, fire, water, wind, earth?
 	
-	public AbilityModel() {
-		
+	public AbilityModel(boolean isRanged, boolean isAOE) { // model for ranged and AOE
+		this.isRanged = isRanged;
+		this.isAOE = isAOE;
+		this.elementsList = new ArrayList<>();
+	}
+	
+	public AbilityModel(boolean isRanged) { // model for ranged
+		this.isRanged = isRanged;
+		this.elementsList = new ArrayList<>();
+	}
+	
+	public AbilityModel() { // model for close
+		this.elementsList = new ArrayList<>();
 	}
 
 }
