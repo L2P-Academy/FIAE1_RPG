2c2f417739d959fd8da76f8fa9025612250d8d42
LvDenu <d2041827@gmail.com>
2024-05-03 11:21:59 +0200

    add function (show,hide,add,remove)

--- FIAE1_RPG/src/controller/InventoryController.java
+++ FIAE1_RPG/src/controller/InventoryController.java
@@ -1,5 +1,37 @@
 package controller;
 
-public class InventoryController {
+import model.ItemModel;
+import view.InventoryView;
+import java.util.ArrayList;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
+public class InventoryController {
+    private InventoryView view;
+    private ArrayList<ItemModel> inventory; 
+
+    // create new inventory and keylistener i and escape 
+    public InventoryController() {
+        this.view = new InventoryView();
+        this.inventory = new ArrayList<ItemModel>(); 
+    }
+    
+    // function to show the inventory
+    public void showInventory() {
+    	view.setVisible(true);
+    }
+    
+    // function to hide inventory
+    public void hideInventory() {
+    	view.setVisible(false);
+    }
+
+    // function to add and remove items
+    public void addItem(ItemModel item) {
+        this.inventory.add(item);
+    }
+
+    public void removeItem(ItemModel item) {
+        this.inventory.remove(item);
+    }
 }
