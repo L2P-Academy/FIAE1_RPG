04468986a8068a4dce0ff3bc31fb0cbad0d2a661
Mlieh <marcel.liehnen@t-online.de>
2024-05-03 11:33:36 +0200

    Klasse CombatView erstellt

---
+++ FIAE1_RPG/src/view/CombatView.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class CombatView extends JFrame{
+	
+	JLabel combatImgLbl, enemyNameLbl, playerNameLbl;
+	
+	
+	public CombatView() {
+		
+	}
+
+}
