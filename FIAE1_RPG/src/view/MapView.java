31b4721ee4d3c4ca5603acb04de09c1c4df2d8d7
MaximilianGietz <maxgietz@web.de>
2024-05-06 18:45:31 +0200

    Halloo
    ich habe den Settingsbutton hinzugefügt, und ihn verändert. Zu einem
    Zahnrad.

--- FIAE1_RPG/src/view/MapView.java
+++ FIAE1_RPG/src/view/MapView.java
@@ -18,8 +18,11 @@ public class MapView extends JFrame {
 
 	JPanel buttonPanel, mapPanel, mainPanel;
 	JLabel mapTitleLbl;
-	JButton mapBtn, inventoryBtn, characterBtn, questsBtn;
-	String mapImagePath = "res/img/map_start.jpg";
+	JButton mapBtn, inventoryBtn, characterBtn, questsBtn, settingsBtn; ///////
+	String mapImagePath = "res/img/MapViewImages/map_start.jpg";
+	String gearImagePath = "C:\\Users\\User\\git\\FIAE1_DnD\\FIAE1_RPG\\res\\img\\MapViewImages\\icon_settings3.png"; //////
+	String buttonPanelBackgroundPath = "res/img/MapViewImages/dnd_background.jpg"; ///////
+	
 	Font gameFont;
 	
 	public MapView() {
@@ -27,14 +30,22 @@ public class MapView extends JFrame {
 		setTitle("Weltkarte");
 		setSize(1200, 800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	//Create Panels
+		//create mainPanel
+		mainPanel = new JPanel(new BorderLayout());
 		
-		//create panels		
-		buttonPanel = new JPanel(new FlowLayout());
+		//create buttonPanel
+		//buttonPanel = new JPanel(new FlowLayout());
+		buttonPanel = new BackGroundPanel(new ImageIcon(buttonPanelBackgroundPath).getImage());
+		buttonPanel.setLayout(new FlowLayout());
+		
+		//create mapPanel
 		mapPanel = new BackGroundPanel(new ImageIcon(mapImagePath).getImage());
 		mapPanel.setLayout(new BorderLayout());
 		mainPanel = new JPanel(new BorderLayout());
-		
-		//create buttons
+				
+	//Create Buttons
 		gameFont = new Font("Old English Text MT", Font.BOLD, 32);
 		mapBtn = new JButton("Weltkarte"); 
 		mapBtn.setFont(gameFont);
@@ -44,12 +55,18 @@ public class MapView extends JFrame {
 		characterBtn.setFont(gameFont);
 		questsBtn = new JButton("Quests");
 		questsBtn.setFont(gameFont);
-		
-		// add buttons to buttonpanel		
+		////Create Gear-Icon for Settings-Button
+		ImageIcon gearIcon = new ImageIcon(new ImageIcon("C:\\Users\\User\\git\\FIAE1_DnD\\FIAE1_RPG\\res\\img\\MapViewImages\\icon_settings3.png").getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH));;
+		////Create the SettingsBtn with GearIcon
+		settingsBtn = new JButton(gearIcon);
+				
+		// add buttons to buttonPanel		
 		buttonPanel.add(mapBtn);
 		buttonPanel.add(inventoryBtn);
 		buttonPanel.add(characterBtn);
 		buttonPanel.add(questsBtn);
+		////add settingsBtn to buttonPanel
+		buttonPanel.add(settingsBtn);
 		
 		// add panels to Window
 		mainPanel.add(buttonPanel, BorderLayout.NORTH);
@@ -59,5 +76,23 @@ public class MapView extends JFrame {
 		setVisible(true);
 			
 	}
-	
+	//Custom JPanel with background image
+	class BackGroundPanel extends JPanel {
+	 private Image backgroundImage;
+
+	 public BackGroundPanel(Image backgroundImage) {
+	     this.backgroundImage = backgroundImage;
+	 }
+
+	 @Override
+	 protected void paintComponent(Graphics g) {
+	     super.paintComponent(g);
+	     if (backgroundImage != null) {
+	         g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+	     }
+	 }
+	}	
 }
+
+
+
