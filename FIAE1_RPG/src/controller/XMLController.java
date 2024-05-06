cc2ca34fe87a57f358555b6486a7d12ac182aac3
christian-gassmann <c-g-93@outlook.de>
2024-05-06 18:46:04 +0200

    Adding formatierung,xslt. xmlWriteInteroryList - first Steps.

--- FIAE1_RPG/src/controller/XMLController.java
+++ FIAE1_RPG/src/controller/XMLController.java
@@ -6,54 +6,33 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
 import java.io.File;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class XMLController {
+import model.ItemModel;
 
+
+
+
+
+public class XMLController {
+	// Adding Filepaths 
+	public String characterInfoFilepath = "res/xml/characterInfo.xml";
+	public String inventoryListFilepath = "res/xml/inventoryList.xml";
+	public String progressFilepath= "res/xml/progress.xml";
+	
+	//Example for an ItemModel
+	
+
+	
 		
+	
 	public void xmlWriteCharacterInfo(){	
 		
-		//creating a xml Document Builder from the DocumentBuilderFactory
-				//try catch is neccessary 
-				//the first 3 prompts creates the needed tree structure in the document
-				try {
-					DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-					DocumentBuilder docBuilder = dbFactory.newDocumentBuilder();	
-					Document document = docBuilder.newDocument();
-					
-					//Adding Root Element 
-					Element rootElement = document.createElement("characterInfo");
-					document.appendChild(rootElement);
-					
-					//Adding several Elements
-					Element name = document.createElement("Name");
-					name.appendChild(document.createTextNode("Chris"));
-					rootElement.appendChild(name);
-					
-					Element healthElement = document.createElement("HealthPoints");
-					healthElement.appendChild(document.createTextNode("100"));
-					rootElement.appendChild(healthElement);
-					
-					
-					
-					
-//					//write the content into the xml File
-//					TransformerFactory tFactory = TransformerFactory.newInstance();
-//					Transformer transformer = tFactory.newTransformer();
-//					DOMSource source = new DOMSource(document);
-//					StreamResult result = new StreamResult(new File("E:\\Projects\\workspace\\dnd_Testing\\characterInfo.xml"));
-//					transformer.transform(source, result);
-//					
-//					 // Output to console for testing
-//			         StreamResult consoleResult = new StreamResult(System.out);
-//			         transformer.transform(source, consoleResult);
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
 	}
 	public void xmlReadCharacterInfo() {
 		
@@ -63,8 +42,70 @@ public class XMLController {
 	}
 	public void xmlReadProgress() {		
 	}
-	public void xmlWriteInventoryList() {
+	public void xmlWriteInventoryList(ItemModel itemModel) {
 		
+		try {
+			//Building steps for a Document
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			Document document = documentBuilder.newDocument();
+			
+			
+			//Adding root Element
+			Element rootElement = document.createElement("InventoryList");
+			document.appendChild(rootElement);
+			
+			//Adding name Child and "root" for every attribute
+			Element nameChild = document.createElement(itemModel.getItemName());
+			rootElement.appendChild(nameChild);
+			
+			//Adding durability Child
+			Element durabilityChild = document.createElement("Durability");
+			durabilityChild.appendChild(document.createTextNode(String.valueOf(itemModel.getItemDurability())));
+			nameChild.appendChild(durabilityChild);
+			
+			//Adding quantity Child
+			Element quantityChild = document.createElement("Quantity");
+			quantityChild.appendChild(document.createTextNode(String.valueOf(itemModel.getItemQuantity())));
+			nameChild.appendChild(quantityChild);
+			
+			//Adding price Child
+			Element priceChild = document.createElement("Price");
+			priceChild.appendChild(document.createTextNode(String.valueOf(itemModel.getItemPrice())));
+			nameChild.appendChild(priceChild);
+			
+			//Adding damage Child
+			Element damageChild = document.createElement("Damage");
+			damageChild.appendChild(document.createTextNode(String.valueOf(itemModel.getItemDamage())));
+			nameChild.appendChild(damageChild);
+			
+			//Adding isQuestItem Child
+			Element isQuestItemChild = document.createElement("QuestItem");
+			isQuestItemChild.appendChild(document.createTextNode(String.valueOf(itemModel.isQuestItem())));
+			nameChild.appendChild(isQuestItemChild);
+			
+			//Adding category Child
+			Element categoryChild = document.createElement("Category");
+			categoryChild.appendChild(document.createTextNode(String.valueOf(itemModel.getCategory())));
+			nameChild.appendChild(categoryChild);
+			
+			
+			// Write Content into the File
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			Transformer transformer = tFactory.newTransformer(new StreamSource(new File("res/xml/formatierung.xslt")));
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(inventoryListFilepath));
+			transformer.transform(source, result);
+			
+			
+			// Console output
+			StreamResult consoleResult = new StreamResult(System.out);
+	        transformer.transform(source, consoleResult);
+		
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	public void xmlReadInventoryList() {
 		
