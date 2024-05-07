0df0ce0f13566a2495c00f20ae139d46d7e641e1
Rakinos89 <rakinos89@yahoo.com>
2024-05-07 20:43:03 +0200

    started the combat controller, just a few lines of code...

--- FIAE1_RPG/src/controller/CombatController.java
+++ FIAE1_RPG/src/controller/CombatController.java
@@ -1,5 +1,36 @@
 package controller;
 
-public class CombatController {
+import java.util.ArrayList;
 
+import model.CharacterModel;
+
+public class CombatController {
+	private ArrayList<CharacterModel> combatants;
+	private int currentTurnIndex;
+
+	// Constructor
+	public CombatController() {
+		combatants = new ArrayList<>();
+		currentTurnIndex = 0;
+	}
+
+	// Method to add a combatant to the encounter
+	public void addCombatant(CharacterModel combatant) {
+		combatants.add(combatant);
+	}
+
+	// Method to start the combat encounter
+	public void startEncounter() {
+		// TODO: under construction...
+	}
+
+	// Method to end the combat encounter
+	public void endEncounter() {
+		// TODO: somethings missing...
+	}
+
+	// Method to handle a combatant's turn
+	private void takeTurn() {
+		// TODO: will be back soon...
+	}
 }
