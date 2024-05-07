b46acc73a26e3383573397834932e437a7a4e3c6
Velsabor <rfnew22@outlook.de>
2024-05-07 21:53:01 +0200

    BossModel Constructor mit Boss-Beispiele

--- FIAE1_RPG/src/model/BossModel.java
+++ FIAE1_RPG/src/model/BossModel.java
@@ -1,17 +1,34 @@
 package model;
 
-import java.util.List;
 
-public class BossModel {
-    private List<MobModel> minions;
-    private int phase;
-
-    public BossModel(List<MobModel> minions, int phase) {
-        this.minions = minions;
-        this.phase = phase;
-    }
-
-    // Methoden entsprechend dem UML-Diagramm
+public class BossModel extends MobModel {
+	//   private List<MobModel> minions;
+ private int phase;
+ 
+ 
+	// Constructor
+ 	public BossModel (String name, String race, int healthPoints, int baseDmg, int baseArmour, boolean isFriendly,
+			int level, boolean isFlying, int phase) {
+ 		super(name, race, healthPoints, baseDmg, baseArmour, isFriendly, level, isFlying);
+ 		this.phase = phase;
+ 	}
+ 	// Beispiel - Bosse
+ 	
+ 	BossModel GoblinKing = new BossModel("Ralphus Bauchuss","Goblin" , 500 , 75, 50, false, 10, false, 3 );
+ 	// Phase 1: bis 60% normal, Phase 2: bis 20% ruft Minions, Phase 3: Unter 20% versucht er zu fliehen
+ 	
+ 	BossModel Dullahan = new BossModel("Todesritter, Dullahan","Untoter", 2000, 150, 150, true, 20, false, 2);
+ 	// Special Minion: Geisterpferd(halb so Stark wie Dullahan)
+ 	// Phase 1: bis 30% kämpft er auf dem Pferd. Phase 2: unter 30% steigt er aus dem Pferd und kämpft bis zum bitteren Ende.
+ 	
+ 	BossModel ErwachsenerDrache = new BossModel("Cadmus Drache der Finsternis", "Drache", 10000, 200, 500, false, 40, true, 3);
+ 	// Phase 1: bis 70% kämpft er auf dem Boden.
+ 	// Phase 2: er fliegt und macht ranged Atacks bis seine Fluegel kapput sind.
+ 	// phase 3: kämpft er in der Rage bis zum Tod. (2x Schaden)
+ 	
+ 	
+ 	
+    // Getter und Setter
     public int getPhase() {
         return phase;
     }
@@ -20,14 +37,7 @@ public class BossModel {
         this.phase = phase;
     }
 
-    public List<MobModel> getMinions() {
-        return minions;
-    }
-
-    public void setMinions(List<MobModel> minions) {
-        this.minions = minions;
-    }
-
+   
     // Beispielmethoden
     public void attack() {
         // Implementierung für den Angriff des Bosses
