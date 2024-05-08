bb07531e861ba63da953add0abd997c4bf78acd7
DSPI163 <DSPI163@PC30175808.cc-college.gecs.loc>
2024-05-08 12:27:20 +0200

    characterName und nameLBL ins Nord Panel verbracht und ausgerichtet. lookElements List und Navigationsbuttons hinzugefügt.

--- FIAE1_RPG/src/view/CharacterCreationView.java
+++ FIAE1_RPG/src/view/CharacterCreationView.java
@@ -1,25 +1,32 @@
 package view;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
 import javax.swing.border.Border;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class CharacterCreationView extends JFrame{
 	
-	JPanel mainPanel, northPanel, southPanel, centerPanel;
+	JPanel mainPanel, northPanel, southPanel, eastPanel, westPanel, centerPanel, namePanel/**/,westBtnPanel/**/, westImgPanel/**/;
 	JTextField characterName;
-	JLabel characterPreview, nameLBL;
-	JButton finishBtn;
+	JLabel characterPreview, nameLBL, testLbl;
+	JButton finishBtn, forwardBtn/**/, backBtn/**/;
+	JList<String> lookElements;
 	Font gameFont, gameFontSmall;
+	
 	
 	public CharacterCreationView() {
 			
@@ -34,33 +41,69 @@ public class CharacterCreationView extends JFrame{
 		// Create the Window-Elements
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		northPanel = new JPanel();
+		northPanel = new JPanel(new GridBagLayout());
 		southPanel = new JPanel();
+		westPanel = new JPanel(new BorderLayout());
+		westBtnPanel = new JPanel(new BorderLayout());
+		westImgPanel = new JPanel();
 		centerPanel = new JPanel();
-		characterName = new JTextField(20); // Set Textfield Width on 20 Characters
-		
+		namePanel = new JPanel();
+		characterName = new JTextField();
 		nameLBL = new JLabel("Gib deinem Charakter einen Namen:");
 		nameLBL.setFont(gameFontSmall);
+		testLbl = new JLabel("Platzhalter");
 		characterPreview = new JLabel("Wähle deinen Charakter");
 		characterPreview.setFont(gameFont);
+		forwardBtn = new JButton("=>");
+		backBtn = new JButton("<=");
 		finishBtn = new JButton("Charakter übernehmen");
 		finishBtn.setFont(gameFont);
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		lookElements = new JList<>(listModel);
 		
-		// Create Label "charakterPreview" and place it on "northPanel"
-		characterPreview.setFont(gameFont);
-		northPanel.add(characterPreview, BorderLayout.CENTER);
+		// Create GridBagLayout and place Elements on northPanel 
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.CENTER; // Set Anchor to CENTER 
+		gbc.gridx = 0; // Set x to 0 
+		gbc.gridy = 0; // Set y to 0
+		gbc.insets.top = 20; // Set Distance to Top (20px)
+		northPanel.add(characterPreview, gbc);
+		
+		// Place nameLBL on northPanel to (0,1)
+		gbc.gridy = 1;
+		northPanel.add(nameLBL, gbc);
+		
+		// Place characterName on northPanel to (0,2)
+		gbc.gridy = 2;
+		gbc.gridwidth = 5;
+		characterName.setColumns(20);
+		northPanel.add(characterName, gbc);
+		
+		// Fill Sampel-Elements into lookElements
+		westPanel.add(lookElements, BorderLayout.CENTER);
+		listModel.addElement("Test1");
+		listModel.addElement("Test2");
+		listModel.addElement("Test3");
+		
+		// Place Buttons on westBtnPanel 
+		westBtnPanel.add(backBtn, BorderLayout.WEST);
+		westBtnPanel.add(forwardBtn, BorderLayout.EAST);
+		
+		// Place westBtnPanel on westPanel
+		westPanel.setBackground(Color.YELLOW);
+		testLbl.setFont(gameFontSmall);
+		westImgPanel.add(testLbl, BorderLayout.CENTER);
+		westPanel.add(westImgPanel, BorderLayout.NORTH);
+		westPanel.add(westBtnPanel, BorderLayout.SOUTH);
 		
 		// Place finishBtn on southPanel
 		southPanel.add(finishBtn, BorderLayout.CENTER);
-		
-		// Create centerPanel and place a Description Label and Name-Textfield on it
-		centerPanel.add(nameLBL, BorderLayout.NORTH);
-		centerPanel.add(characterName, BorderLayout.CENTER);
-		
+	
 		// Place Elements on mainPanel
 		mainPanel.add(northPanel, BorderLayout.NORTH);
 		mainPanel.add(southPanel, BorderLayout.SOUTH);
 		mainPanel.add(centerPanel, BorderLayout.CENTER);
+		mainPanel.add(westPanel, BorderLayout.WEST);
 		
 		finishBtn.addActionListener(new ActionListener() {
 			
