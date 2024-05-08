9eb4e9046c452c5f3eb68552d0d936d7aa252c2a
Pandaoria <seifert.michael@outlook.com>
2024-05-08 12:01:42 +0200

    QuestView Coder etwas geordnet, ItemBild kann eingefügt werden

--- FIAE1_RPG/src/view/QuestView.java
+++ FIAE1_RPG/src/view/QuestView.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -13,13 +14,29 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import controller.QuestController;
+
 public class QuestView extends JFrame{
 	
-	JLabel questTitleLbl, questDescriptionLbl, questImgLbl;
+	//Controller
+	QuestController questController;
+	
+	//Label
+	JLabel questTitleLbl, questDescLbl, questImgLbl;
+	
+	//Buttons
 	JButton acceptQuestBtn, exitQuestBtn;
-	JPanel buttonPnl, questPnl, questItemPnl, questTitlePnl, mainPnl;
+	
+	//Panel
+	JPanel buttonPnl, questBgPnl, questItemPnl, questTitlePnl, mainPnl;
+	
+	//Background
 	String questBackgroundPath = "res/img/QuestDescBack.png";
+	String questItemBackground = "res/img/QuestItem_Placeholder.png";
+	
+	//Font
 	Font gameFont;
+	Font questDesc;
 	
 	public QuestView() {
 		
@@ -29,27 +46,45 @@ public class QuestView extends JFrame{
 		
 		//Create Font
 		gameFont = new Font("Old English Text MT", Font.BOLD, 64);
+		questDesc = new Font("Old English Text MT", Font.BOLD, 32);
 		
 		//Create Label
 		questTitleLbl = new JLabel("Hier kommt der Quest Titel hin");
-		questDescriptionLbl = new JLabel("Hier kommt die Quest Beschreibung hin");
-		questImgLbl = new JLabel("Hier kommt das Quest Bild hin");
+		questTitleLbl.setFont(gameFont);
+		questDescLbl = new JLabel("Hier kommt die Quest Beschreibung hin");
+		questDescLbl.setFont(questDesc);
+		//questImgLbl = new JLabel("Hier kommt das Quest Bild hin");
+		//questImgLbl.setFont(gameFont);
 		
-		//Create Panel
+		//Button Panel
 		buttonPnl = new JPanel(new FlowLayout());
-		questPnl = new BackGroundPanel(new ImageIcon(questBackgroundPath).getImage());
-		questPnl.setLayout(new BorderLayout());
-		questItemPnl = new JPanel(new BorderLayout());
+		
+		//QuestItem  Panel
+		questItemPnl = new JPanel(new FlowLayout());
+		questItemPnl.setLayout(new FlowLayout(FlowLayout.CENTER));
+		questItemPnl.setPreferredSize(new Dimension(250, 500));
+		//questImgLbl.setLayout(new FlowLayout(FlowLayout.CENTER));
+		//questImgLbl.setPreferredSize(new Dimension(250, 500));
+		
+		//QuestTitle Panel
 		questTitlePnl = new JPanel(new BorderLayout());
-		mainPnl = new JPanel(new BorderLayout());	
 		
-		//Panel Layout (  nicht sicher ob notwendig)
-		//buttonPanel.setLayout(new FlowLayout());
-		//questPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-		//questItemPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		//Main Panel
+		mainPnl = new JPanel(new BorderLayout());
 		
-		//set Panel Background for better Sichtbarkeit
-		questPnl.setBackground(Color.CYAN);
+		//Create ItemBackground
+		questItemPnl = new BackGroundPanel(new ImageIcon(questItemBackground).getImage());
+		questItemPnl.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		//Create BackgroundPanel
+		questBgPnl = new BackGroundPanel(new ImageIcon(questBackgroundPath).getImage());
+		questBgPnl.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		//questDescLbl position
+		questDescLbl.setPreferredSize(new Dimension(625, 350));
+		
+		//set Panel Background for better Sichtbarkeit und Beautiness
+		questBgPnl.setBackground(new Color(172, 134, 70));
 		buttonPnl.setBackground(Color.GREEN);
 		questItemPnl.setBackground(Color.YELLOW);
 		questTitlePnl.setBackground(Color.ORANGE);
@@ -65,17 +100,18 @@ public class QuestView extends JFrame{
 		buttonPnl.add(exitQuestBtn);
 		
 		//Fill Panel with Labels
-		questPnl.add(questDescriptionLbl);
-		questItemPnl.add(questImgLbl);
+		questBgPnl.add(questDescLbl);
+		//questItemPnl.add(questImgLbl);
 		questTitlePnl.add(questTitleLbl);
 		
 		//Set Position for Panels
 		mainPnl.add(questTitlePnl, BorderLayout.NORTH);
-		mainPnl.add(questPnl, BorderLayout.CENTER);
+		mainPnl.add(questBgPnl, BorderLayout.CENTER);
 		mainPnl.add(buttonPnl, BorderLayout.SOUTH);
 		mainPnl.add(questItemPnl, BorderLayout.WEST);
 		
 		getContentPane().add(mainPnl);
+		
 		
 		//give function to Buttons
 		acceptQuestBtn.addActionListener(new ActionListener() {
@@ -83,9 +119,7 @@ public class QuestView extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				MapView mapView = new MapView();
-				dispose();
-				
-				
+				dispose();	
 			}
 		});
 		
@@ -94,10 +128,10 @@ public class QuestView extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				MapView mapView = new MapView();
-				dispose();
-				
+				dispose();	
 			}
 		});
+		
 		
 		setLocationRelativeTo(null);
 		setVisible(true);
