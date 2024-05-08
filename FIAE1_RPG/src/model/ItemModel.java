8c1f70b977ae189adb543853a395bac075e3b9b0
weirdmustard <samael.schroeder@mail.de>
2024-05-08 12:40:58 +0200

    updated version + added items

--- FIAE1_RPG/src/model/ItemModel.java
+++ FIAE1_RPG/src/model/ItemModel.java
@@ -12,11 +12,14 @@ public class ItemModel {
 
 	// Item-Liste:
 	// https://more2learn-my.sharepoint.com/:x:/g/personal/ssch467_more2learn_de/EcYh5wDUl6xOkwiXIFZANVQB8GkH7YTvfICZ9erPfrlPkA?e=BddI87
+	//
+	// (enthält auch Infos zu consumable/usable + Description für Views)
 
-	// TO-DO 08.05.
+	// TO-DO 10.05.
 	//
 	//
-	//
+
+	// Constructors
 
 	// Standard Constructor
 	public ItemModel(String itemName, int itemQuantity, double itemPrice, int itemDamage, boolean isQuestItem) {
@@ -41,8 +44,6 @@ public class ItemModel {
 		this.isQuestItem = false;
 		this.itemDamage = itemDamage;
 	}
-	
-	
 
 	// Getter & Setter
 
@@ -86,18 +87,79 @@ public class ItemModel {
 		this.isQuestItem = isQuestItem;
 	}
 
-// Item-Liste
-
-	// Item-Liste:
+	//
+	// --------------------------------------------------------------------------
+	// Item-Liste
+	//
 	// https://more2learn-my.sharepoint.com/:x:/g/personal/ssch467_more2learn_de/EcYh5wDUl6xOkwiXIFZANVQB8GkH7YTvfICZ9erPfrlPkA?e=BddI87
+	//
+	// ItemModel itemName = new ItemModel "Bezeichnung",
+	// itemQuantity,itemPrice,itemDamage,isQuestItem
 
-	// itemName,itemQuantity,itemPrice,itemDamage,isQuestItem
+	ItemModel Gold = new ItemModel("Gold", 1, 1, 0, false);
 
-	ItemModel item2 = new ItemModel("Stein", 1, 0.1, 5, false);
+	ItemModel KleinerHeiltrank = new ItemModel("Kleiner Heiltrank", 1, 20, 0, false);
 
-	ItemModel item3 = new ItemModel("Heiltrank", 1, 20, 0, false);
+	ItemModel KleinerManatrank = new ItemModel("Kleiner Manatrank", 1, 20, 0, false);
 
-	ItemModel item5 = new ItemModel("Seil", 1, 5, 0, false);
+	ItemModel KleinerStaminatrank = new ItemModel("Kleiner Staminatrank", 1, 20, 0, false);
 
-	ItemModel item6 = new ItemModel("Banditenkopf", 0, 0, 0, true);
+	ItemModel Heiltrank = new ItemModel("Heiltrank", 1, 50, 0, false);
+
+	ItemModel Manatrank = new ItemModel("Manatrank", 1, 50, 0, false);
+
+	ItemModel Staminatrank = new ItemModel("Staminatrank", 1, 50, 0, false);
+
+	ItemModel GrosserHeiltrank = new ItemModel("Großer Heiltrank", 1, 85, 0, false);
+
+	ItemModel GrosserManatrank = new ItemModel("Großer Manatrank", 1, 85, 0, false);
+
+	ItemModel GrosserStaminatrank = new ItemModel("Großer Staminatrank", 1, 85, 0, false);
+
+	ItemModel Wiederbelebungstrank = new ItemModel("Wiederbelebungstrank", 1, 190, 0, false);
+
+	ItemModel Brot = new ItemModel("Brot", 1, 5, 0, false);
+
+	ItemModel Zimtschnecke = new ItemModel("Zimtschnecke", 1, 12, 0, false);
+
+	ItemModel Weintrauben = new ItemModel("Weintrauben", 1, 10, 0, false);
+
+	ItemModel Fisch = new ItemModel("Fisch", 1, 7, 0, false);
+
+	ItemModel Kebab = new ItemModel("Kebab", 1, 25, 0, false);
+
+	ItemModel Ei = new ItemModel("Ei", 1, 3, 0, false);
+
+	ItemModel GoldEi = new ItemModel("Gold-Ei", 1, 200, 0, false);
+
+	ItemModel HandVollWuermer = new ItemModel("Hand voll Würmer", 1, 1, 0, false);
+
+	ItemModel EinStueckKuchen = new ItemModel("Ein Stück Kuchen", 1, 20, 0, false);
+
+	ItemModel AnanasAusDerDose = new ItemModel("Ananas aus der Dose", 1, 4, 0, false);
+
+	ItemModel Scheiblettenkaese = new ItemModel("Scheiblettenkäse", 1, 4, 0, false);
+
+	ItemModel ScheibeFormschinken = new ItemModel("Scheibe Formschinken", 1, 4, 0, false);
+
+	ItemModel ScheibeToast = new ItemModel("Scheibe Toast", 1, 4, 0, false);
+
+	ItemModel Seil = new ItemModel("Seil", 1, 5, 0, false);
+
+	ItemModel Stock = new ItemModel("Stock", 1, 2, 0, false);
+
+	ItemModel HaeufchenAsche = new ItemModel("Häufchen Asche", 1, 1, 0, false);
+
+	ItemModel Knoepfe = new ItemModel("Knöpfe", 1, 1, 0, false);
+
+	ItemModel EtwasDreck = new ItemModel("Etwas Dreck", 1, 1, 0, false);
+
+	ItemModel Knochen = new ItemModel("Knochen", 1, 3, 0, false);
+
+	ItemModel Eingeweide = new ItemModel("Eingeweide", 1, 5, 0, false);
+
+	ItemModel Monsterleber = new ItemModel("Monsterleber", 1, 5, 0, false);
+
+	ItemModel Banditenkopf = new ItemModel("Banditenkopf", 1, 0, 0, true);
+
 }
