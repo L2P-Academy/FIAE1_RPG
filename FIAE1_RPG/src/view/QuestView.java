695e05efd4188a55d132ec8fee40444ca5553e48
Pandaoria <seifert.michael@outlook.com>
2024-05-09 15:12:59 +0200

    Button sounds, Desc Scroll hinzugefügt, Quest Abruf funkt.

--- FIAE1_RPG/src/view/QuestView.java
+++ FIAE1_RPG/src/view/QuestView.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,16 +14,23 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import controller.QuestController;
+import controller.SoundController;
+import model.QuestModel;
 
 public class QuestView extends JFrame{
 	
 	//Controller
 	QuestController questController;
 	
+	//Model
+	QuestModel questModel;
+	
 	//Label
-	JLabel questTitleLbl, questDescLbl, questImgLbl;
+	JLabel questTitleLbl, questDescLbl, questImgLbl, questItemLbl;
 	
 	//Buttons
 	JButton acceptQuestBtn, exitQuestBtn;
@@ -30,31 +38,65 @@ public class QuestView extends JFrame{
 	//Panel
 	JPanel buttonPnl, questBgPnl, questItemPnl, questTitlePnl, mainPnl;
 	
+	//textarea
+	JTextArea questDescArea;
+	
+	//Scroll
+	JScrollPane scrollPane;
+	
 	//Background
 	String questBackgroundPath = "res/img/QuestDescBack.png";
 	String questItemBackground = "res/img/QuestItem_Placeholder.png";
+	String questTitleBackground = "res/img/QuestTitleBackground.png";
 	
 	//Font
 	Font gameFont;
 	Font questDesc;
+	Font questTitle;
+	
+	//Sounds
+	SoundController soundController;
 	
 	public QuestView() {
 		
+		
+		//Test Quest
+		QuestModel questModel = new QuestModel("Test Titel", "Vor langer Zeit verschwand das magische Artefakt aus dem Königreich und seine Spur verblasste im Nebel der Zeit. Die Bewohner des Landes sehnen sich nach seiner Rückkehr und die Königin hat eine Belohnung ausgesetzt für jene, die es finden. Mutige Abenteurer werden gesucht, um die Geheimnisse des verschwundenen Artefakts zu enthüllen und das Land vor dem Verfall zu retten. Doch der Weg zum Ruhm ist gefährlich, und nur die Tapfersten werden sich der Herausforderung stellen.", 1, true, 100.0, 50, null, 1, null, 0, 10);
+		
+		
+		//Create Window
 		setTitle("Neue Quest!");
 		setSize(1200, 800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		soundController = new SoundController();
 		
 		//Create Font
-		gameFont = new Font("Old English Text MT", Font.BOLD, 64);
+		gameFont = new Font("Old English Text MT", Font.BOLD, 32);
 		questDesc = new Font("Old English Text MT", Font.BOLD, 32);
+		questTitle = new Font("Old English Text MT", Font.BOLD, 32);
 		
-		//Create Label
-		questTitleLbl = new JLabel("Hier kommt der Quest Titel hin");
-		questTitleLbl.setFont(gameFont);
-		questDescLbl = new JLabel("Hier kommt die Quest Beschreibung hin");
-		questDescLbl.setFont(questDesc);
-		//questImgLbl = new JLabel("Hier kommt das Quest Bild hin");
-		//questImgLbl.setFont(gameFont);
+		//Create QuestTitle Label
+		questTitleLbl = new JLabel();
+        questTitleLbl.setText(questModel.getTitle());
+		questTitleLbl.setFont(questTitle);
+		
+		//Create Quest Desc Area with Scroll
+		questDescArea = new JTextArea(questModel.getDescription());
+		questDescArea.setFont(questDesc);
+		questDescArea.setLineWrap(true);
+		questDescArea.setWrapStyleWord(true);
+		scrollPane = new JScrollPane(questDescArea);
+		scrollPane.setLocation(scrollPane.getX(), scrollPane.getY() + 100);
+		scrollPane.setBounds(scrollPane.getX(), scrollPane.getY() + 100, scrollPane.getWidth(), scrollPane.getHeight());
+		
+		
+		//Create ItemLabel From Model
+	    questItemLbl = new JLabel();
+	    if (questModel.getQuestItemRequired() != null) {
+	    questItemLbl.setText(questModel.getQuestItemRequired().getItemName());
+	    } else {
+	    questItemLbl.setText("Kein Quest-Item erforderlich");	     }
+
 		
 		//Button Panel
 		buttonPnl = new JPanel(new FlowLayout());
@@ -63,8 +105,7 @@ public class QuestView extends JFrame{
 		questItemPnl = new JPanel(new FlowLayout());
 		questItemPnl.setLayout(new FlowLayout(FlowLayout.CENTER));
 		questItemPnl.setPreferredSize(new Dimension(250, 500));
-		//questImgLbl.setLayout(new FlowLayout(FlowLayout.CENTER));
-		//questImgLbl.setPreferredSize(new Dimension(250, 500));
+
 		
 		//QuestTitle Panel
 		questTitlePnl = new JPanel(new BorderLayout());
@@ -80,19 +121,27 @@ public class QuestView extends JFrame{
 		questBgPnl = new BackGroundPanel(new ImageIcon(questBackgroundPath).getImage());
 		questBgPnl.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
-		//questDescLbl position
-		questDescLbl.setPreferredSize(new Dimension(625, 350));
+		//Create questTitle Background
+		questTitlePnl = new BackGroundPanel(new ImageIcon(questTitleBackground).getImage());
+		questTitlePnl.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		
+		// set ScrollPane Size
+		scrollPane.setPreferredSize(new Dimension(600, 340));
+		
+		//questTitleLbl postition
+		questTitleLbl.setPreferredSize(new Dimension(1000,65));
 		
 		//set Panel Background for better Sichtbarkeit und Beautiness
-		questBgPnl.setBackground(new Color(172, 134, 70));
-		buttonPnl.setBackground(Color.GREEN);
-		questItemPnl.setBackground(Color.YELLOW);
-		questTitlePnl.setBackground(Color.ORANGE);
+		questBgPnl.setBackground(new Color(190, 196, 168));
+		buttonPnl.setBackground(new Color(190, 196, 168));
+		questItemPnl.setBackground(new Color(190, 196, 168));
+		questTitlePnl.setBackground(new Color(190, 196, 168));
 		
 		//Create Button
-		acceptQuestBtn = new JButton("Wenns sein muss");//Worktitle
+		acceptQuestBtn = new JButton("Mit freuden, mein Edler");//Worktitle
 		acceptQuestBtn.setFont(gameFont);
-		exitQuestBtn = new JButton("Ne du, keine Lust");//Worktitle
+		exitQuestBtn = new JButton("Oh nein, mein Edler");//Worktitle
 		exitQuestBtn.setFont(gameFont);
 		
 		//Fill Button
@@ -100,9 +149,9 @@ public class QuestView extends JFrame{
 		buttonPnl.add(exitQuestBtn);
 		
 		//Fill Panel with Labels
-		questBgPnl.add(questDescLbl);
-		//questItemPnl.add(questImgLbl);
+		questBgPnl.add(scrollPane);
 		questTitlePnl.add(questTitleLbl);
+		questItemPnl.add(questItemLbl);
 		
 		//Set Position for Panels
 		mainPnl.add(questTitlePnl, BorderLayout.NORTH);
@@ -118,7 +167,9 @@ public class QuestView extends JFrame{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				soundController.playButtonClickSound();
 				MapView mapView = new MapView();
+				soundController.stopMusicLoop();
 				dispose();	
 			}
 		});
@@ -127,7 +178,9 @@ public class QuestView extends JFrame{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				soundController.playButtonClickSound();
 				MapView mapView = new MapView();
+				soundController.stopMusicLoop();
 				dispose();	
 			}
 		});
@@ -136,6 +189,8 @@ public class QuestView extends JFrame{
 		setLocationRelativeTo(null);
 		setVisible(true);
 		
+		//play Music in background after rendering		
+		soundController.playMusicLoop("res/soundFX/ambientSounds/village_ambient.wav");
 	}
 
 }
