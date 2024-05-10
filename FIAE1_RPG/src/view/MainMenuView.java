f55b1256b872fbc78eee6be270a8cbb7219f1404
L2P-Academy <projekte@l2p-academy.com>
2024-05-10 16:05:15 +0200

    MainMenu improved, ID added

--- FIAE1_RPG/src/view/MainMenuView.java
+++ FIAE1_RPG/src/view/MainMenuView.java
@@ -1,112 +1,129 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
 import controller.SoundController;
+import model.SerializationIDs;
 
-public class MainMenuView extends JFrame{
+public class MainMenuView extends JFrame {
+
+	private static final long serialVersionUID = SerializationIDs.mainMenuViewID;
 	
-	JLabel gameTitleLbl;
-	JPanel backgroundPanel, buttonPanel;
-	JButton newGameBtn, loadGameBtn, closeGameBtn;
-	String introImgPath = "res/img/Intro_Picture.jpg";
-	ImageIcon introImgIcon;
-	Font gameFont;
-	SoundController soundController;
-	
+	private JLabel gameTitleLbl;
+	private JPanel backgroundPanel, buttonPanel;
+	private JButton newGameBtn, loadGameBtn, closeGameBtn;
+	private String introImgPath = "res/img/Intro_Picture.jpg";
+	private Font gameFont;
+	private SoundController soundController;
+
 	public MainMenuView() {
-		
-		setTitle("Dragons & Dungeons");
+
+		// initialize
+		setTitle("Dungeons & Legends");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setUndecorated(true);
 		soundController = new SoundController();
-		
+
 		// create background Panel
 		backgroundPanel = new BackGroundPanel(new ImageIcon(introImgPath).getImage());
 		backgroundPanel.setLayout(new BorderLayout());
-		
+
 		// create JLabel for the Game Title
 		gameFont = new Font("Old English Text MT", Font.BOLD, 64);
-		gameTitleLbl = new JLabel("Dragons & Dungeons!", SwingConstants.CENTER);
+		gameTitleLbl = new JLabel("Dungeons & Legends!", SwingConstants.CENTER);
 		gameTitleLbl.setFont(gameFont);
 		gameTitleLbl.setForeground(new Color(245, 245, 220));
-		
+
 		// button panel for better Alignment
 		buttonPanel = new JPanel(new FlowLayout());
-		
+
 		// create buttons and assign Fonts
-		newGameBtn = new JButton("Neues Spiel");		
-		newGameBtn.setFont(gameFont);		
-		loadGameBtn = new JButton("Spiel laden");		
-		loadGameBtn.setFont(gameFont);		
+		newGameBtn = new JButton("Neues Spiel");
+		beautifyButton(newGameBtn);
+		loadGameBtn = new JButton("Spiel laden");
+		beautifyButton(loadGameBtn);
 		closeGameBtn = new JButton("Beenden");
-		closeGameBtn.setFont(gameFont);
+		beautifyButton(closeGameBtn);
 
 		// fill buttonPanel with Buttons
 		buttonPanel.add(newGameBtn);
 		buttonPanel.add(loadGameBtn);
 		buttonPanel.add(closeGameBtn);
-		
+		buttonPanel.setOpaque(false);
+
 		// fill the background with contents
 		backgroundPanel.add(gameTitleLbl, BorderLayout.NORTH);
 		backgroundPanel.add(buttonPanel, BorderLayout.SOUTH);
-		
+
 		newGameBtn.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				soundController.playButtonClickSound();
 				soundController.stopMusicLoop();
-				CharacterCreationView characterCreationView = new CharacterCreationView();				
-				dispose();								
+				CharacterCreationView characterCreationView = new CharacterCreationView();
+				dispose();
 			}
 		});
-		
+
 		loadGameBtn.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				soundController.playButtonClickSound();
 				SavegameView savegameView = new SavegameView();
-				
+
 			}
 		});
-		
+
 		closeGameBtn.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				soundController.playButtonClickSound();
 				soundController.stopMusicLoop();
 
 				dispose();
-				
+
 			}
 		});
-		
+
 		getContentPane().add(backgroundPanel);
-		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
-		
-		//play Music in background after rendering		
+
+		// play Music in background after rendering
 		soundController.playMusicLoop("res/soundFX/music/mainMenu_music.wav");
-		
+
 	}
 
+	// Modify Buttons
+	private void beautifyButton(JButton button) {
+		button.setFocusPainted(false);
+		button.setBackground(new Color(10, 50, 100));
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("Old English Text MT", Font.BOLD, 36));
+
+		// Rounded Corners
+		Border border = BorderFactory.createLineBorder(new Color(255, 255, 255), 2);
+		Border roundedBorder = BorderFactory.createCompoundBorder(border,
+				BorderFactory.createEmptyBorder(10, 20, 10, 20));
+		button.setBorder(
+				BorderFactory.createCompoundBorder(roundedBorder, BorderFactory.createEmptyBorder(5, 15, 5, 15)));
+
+		// color change when MouseOver is happening
+		button.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseEntered(java.awt.event.MouseEvent evt) {
+				button.setBackground(new Color(100, 149, 237));
+			}
+
+			public void mouseExited(java.awt.event.MouseEvent evt) {
+				button.setBackground(new Color(10, 50, 100));
+			}
+		});
+	}
 }
