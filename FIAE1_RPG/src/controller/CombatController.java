4714cb1146b7867f1eed021f07ab43ce7e3790d6
Cédric Ant. <ant.cedric91@gmail.com>
2024-07-31 12:27:06 +0200

    Kurzes Update, damit ich die aktuelle Version des Projekts holen kann.

--- FIAE1_RPG/src/controller/CombatController.java
+++ FIAE1_RPG/src/controller/CombatController.java
@@ -2,7 +2,12 @@ package controller;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+
+import model.NpcModel;
 import model.PlayerCharacterModel;
+import model.NpcModel.NpcCategory;
+import view.CombatView;
 
 public class CombatController {
 	private ArrayList<PlayerCharacterModel> combatants;
@@ -44,5 +49,52 @@ public class CombatController {
 			currentTurnIndex = 0;
 		}
 		takeTurn();
+	}
+	
+	// // // Ab hier hat Cedric rumgefuschelt
+	private List<NpcModel> enemiesDatabase;
+	
+	// Constructor for randomBattle
+	public CombatController(List<NpcModel> enemiesDatabase) {
+		this.enemiesDatabase = enemiesDatabase;
+	}
+	
+	// random Encounter method
+	private void encounter(int battlemaps, int... npcID) {
+		List<NpcModel> encounterEnemies = new ArrayList<>();
+		currentTurnIndex = 0;
+		
+		// this is a schleife that checks for npcID and adds them to the fight
+		for (int id : npcID) {
+			for (NpcModel enemy : enemiesDatabase) {
+				if (enemy.getNpcID() == id) {
+					encounterEnemies.add(enemy);
+				}
+			}
+		// at this point, combatView starts
+		new CombatView(battlemaps);
+		}
+		
+		// battle starts at this point
+		startBattle(encounterEnemies);
+	}
+	
+	private void startBattle(List<NpcModel> enemies) {
+		// currently just placeholders
+		System.out.println("A battle between the hero and the following monsters has begun:");
+        System.out.println("Hero");
+        for (NpcModel enemy : enemies) {
+            System.out.println(enemies);
+        }
+        // Placeholder for the actual combat logic
+        System.out.println("Combat is simulated here...");
+        System.out.println("The battle has ended.");
+	}
+	
+	private void addMonsters() {
+		// this just adds the existing monster npcs to the combatDatabase, until we work with SQL
+		 NpcModel wolf = new NpcModel(1, 1, 30, 10, 1, 5, null, "Wolf");
+		 NpcModel slime = new NpcModel(2, 1, 60, 20, 2, 32, null, "Schleim");
+		 
 	}
 }
