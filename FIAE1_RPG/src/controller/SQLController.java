55654096e31a57148761fae04b325b09f62e0c99
christian-gassmann <168087906+christian-gassmann@users.noreply.github.com>
2024-08-21 16:04:22 +0200

    E

--- FIAE1_RPG/src/controller/SQLController.java
+++ FIAE1_RPG/src/controller/SQLController.java
@@ -447,14 +447,14 @@ public class SQLController {
 				int rewardXP = resultSet.getInt("RewardXP");
 				int rewardGold = resultSet.getInt("RewardGold");
 				boolean isMainQuest = resultSet.getBoolean("IsMainQuest");
-				boolean isActiv = resultSet.getBoolean("IsActiv");
+				boolean isActive = resultSet.getBoolean("IsActiv");
 				boolean completed = resultSet.getBoolean("IsCompleted");
 				int item = resultSet.getInt("ItemID");
 				String name = resultSet.getString("Name");
 				String description = resultSet.getString("Description");
 				
 				activeQuest = new QuestModel(questID, reqLevel, rewardXP, item, rewardGold, 
-						isMainQuest, isActiv, completed, name, description);
+						isMainQuest, isActive, completed, name, description);
 				
 				
 			}
